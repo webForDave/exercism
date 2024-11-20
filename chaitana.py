215e9fbfb342f04427699a2d5c387079d71e2280
David Akinola <dakinola54@gmail.com>
2024-11-20 19:19:40 +0100

    implementation of dd me with my friends

--- chaitana.py
+++ chaitana.py
@@ -39,7 +39,8 @@ def add_me_with_my_friends(queue, index, person_name):
     :return: list - queue updated with new name.
     """
 
-    pass
+    queue.insert(index, person_name)
+    return queue
 
 
 def remove_the_mean_person(queue, person_name):
