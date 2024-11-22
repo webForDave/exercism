b44a37e858b5119deb71def8627ef65c01a2f2c6
David Akinola <dakinola54@gmail.com>
2024-11-22 03:37:55 +0100

    ISBN verification works

--- ISBN.py
+++ ISBN.py
@@ -1,31 +1,31 @@
 def is_valid(isbn):
     isbn = isbn.replace('-', '')
     isbn_as_list = list(isbn)
+    list_of_chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '0', 'X']
+    digits = []
 
-    if isbn == '':
+    if len(isbn_as_list) == 0:
+        return False
+    elif len(isbn_as_list) > 10 or len(isbn_as_list) < 10:
+        return False 
+    elif 'X' in isbn_as_list and isbn_as_list[-1] != 'X':
         return False
     else:
-        if 'X' in isbn_as_list:
+        if isbn_as_list[-1] == 'X':
             isbn_as_list[-1] = '10'
 
-        new_isbn_list = []
-
-        for number in isbn_as_list:
-            if number != int(number) :
+        for char in isbn_as_list:
+            if char not in list_of_chars:
                 return False
             else:
-                new_isbn_list.append(int(number))
-        for i in new_isbn_list:
-            if i != int(i):
-                print(type(i))
-            else:
-                lenght_of_digits = len(new_isbn_list)
-                multiples = []
+                digits.append(int(char))
 
-                for index, value in enumerate(new_isbn_list):
-                    multiples.append((lenght_of_digits - index) * value)
+        length_of_digits = len(digits)
+        multiplied_vlues = []
 
-                return bool(sum(multiples) % 11 == 0)
+        for index, value in enumerate(digits):
+            multiplied_vlues.append((length_of_digits - index) * value)
 
+        return bool(sum(multiplied_vlues) % 11 == 0)
 
-print(is_valid("3-598-21507-X"))
+print(is_valid("98245726788"))
