19018783a0248f4e077bea06810c5c008e242a96
David Akinola <dakinola54@gmail.com>
2024-11-25 13:59:18 +0100

    removed else statement from logic

--- hamming.py
+++ hamming.py
@@ -1,8 +1,7 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be of equal length.")
-    else:
-        error = 0
-        for a, b in zip(strand_a, strand_b):
-            if a != b: error += 1 
+    error = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b: error += 1 
     return error
