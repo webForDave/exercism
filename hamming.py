244c3cdc337b5be62373cc33d3c297f6e14a6e4d
David Akinola <dakinola54@gmail.com>
2024-11-25 13:55:22 +0100

    return mistakes in DNA strands

---
+++ hamming.py
@@ -0,0 +1,8 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    else:
+        error = 0
+        for a, b in zip(strand_a, strand_b):
+            if a != b: error += 1 
+    return error
