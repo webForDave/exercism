4f5afe1e59f723c79f48bcd617b5edd998383562
David Akinola <dakinola54@gmail.com>
2024-12-04 17:16:47 +0100

    added doctring to abbreviate function

--- acronym.py
+++ acronym.py
@@ -1,4 +1,12 @@
 def abbreviate(words):
+    """Gives the abbreviation of a word.
+    
+    :param words/phrase: str - argument to check acronym.
+    :return: str - abbreviated form of word/phrase.
+
+    Function that takes a group of words, and convert them into the abbreviated form.
+    """
+    
     words = words.replace('-', ' ').replace('_', ' ').title().split()
     acronyms = ''
     for word in words:
