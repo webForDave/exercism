ad8fcb08bfa24456e260d98c42cc170b71ea3787
David Akinola <dakinola54@gmail.com>
2024-12-17 03:36:13 +0100

    removed list comprehension

--- perfect_numbers.py
+++ perfect_numbers.py
@@ -7,12 +7,17 @@ def classify(number):
 
     if number < 1:
         raise ValueError("Classification is only possible for positive integers.")
-    aliquot_numbers = [i for i in range(1, number) if number % i == 0]
+    aliquot_sum = 0
 
-    if sum(aliquot_numbers) > number:
+    for i in range(1, number):
+        if number % i == 0:
+            aliquot_sum += i
+
+    if aliquot_sum > number:
         return 'Abundant'
-    elif sum(aliquot_numbers) < number:
+    elif aliquot_sum < number:
         return 'Deficient'
     return 'Perfect'
     
 
+print(classify(10))
