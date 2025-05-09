6f66aac5c37be4040abe1204c0f4388b41b752fe
David Akinola <dakinola54@gmail.com>
2025-05-09 21:27:18 +0100

    removed loop over every key and vallue in sequence dictionary

--- protein_translation.py
+++ protein_translation.py
@@ -1,34 +1,32 @@
 def proteins(strand):
-    phenylalanine = ['UUU', 'UUC']
-    leucine = ['UUA', 'UUG']
-    methionine = ['AUG']
-    serine = ['UCU', 'UCC', 'UCA', 'UCG']
-    tyrosine = ['UAU', 'UAC']
-    cysteine = ['UGU', 'UGC']
-    tryptophan = ['UGG']
-    stop = ['UAA', 'UAG', 'UGA']
+    seq = {
+    'UUU' : "Phenylalanine",
+    'UUC' : "Phenylalanine",
+    'UUA' : "Leucine",
+    'UUG': "Leucine",
+    'AUG' : "Methionine",
+    'UCU' : "Serine",
+    'UCC': "Serine" ,
+    'UCA' : "Serine",
+    'UCG' : "Serine",
+    'UAU' : "Tyrosine",
+    'UAC' : "Tyrosine",
+    'UGU' : "Cysteine",
+    'UGC' : "Cysteine",
+    'UGG' : "Tryptophan",
+    'UAA' : "Stop",
+    'UAG' : "Stop",
+    'UGA' : "Stop"
+    }
 
-    groups = []
+    amino_acids = []
     codons = [strand[char: char + 3] for char in range(0, len(strand), 3)]
 
     for codon in codons:
-        if codon in phenylalanine:
-            groups.append("Phenylalanine")
-        elif codon in leucine:
-            groups.append("Leucine")
-        elif codon in methionine:
-            groups.append("Methionine")
-        elif codon in serine:
-            groups.append("Serine")
-        elif codon in tyrosine:
-            groups.append("Tyrosine")
-        elif codon in cysteine:
-            groups.append("Cysteine")
-        elif codon in tryptophan:
-            groups.append("Tryptophan")
-        elif codon in stop:
+        if codon in ["UAA", "UAG", "UGA"]:
             break
-    return groups
+        amino_acids.append(seq.get(codon))
 
+    return amino_acids
 
 print(proteins("AUGUUUUCUUAAAUG"))
