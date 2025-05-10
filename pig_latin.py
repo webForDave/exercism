47e7774cb72dfd86f03b7be1e81615466b105d87
David Akinola <dakinola54@gmail.com>
2025-05-10 12:30:05 +0100

    implemented first rule

--- pig_latin.py
+++ pig_latin.py
@@ -3,17 +3,9 @@ def translate(text):
     text_as_list = list(text)
     removed_consnants = []
 
-    if text_as_list[0] in vowels or (text_as_list[0] == 'x' and text_as_list[1] == 'r') or (text_as_list[0] == 'y' and text_as_list[1] == 't'):
-        text_as_list.append('ay')
-    else:
-        for _ in text_as_list:
-            if text_as_list[0] not in vowels:
-                removed_consnants.append(text_as_list[0])
-                text_as_list.pop(0)
-        text_as_list = text_as_list + removed_consnants
+    if text_as_list[0] in vowels or "".join(text_as_list[0:2]) == "xr" or "".join(text_as_list[0:2]) == "yt":
         text_as_list.append('ay')
 
     return ''.join(text_as_list)
 
-
-print(translate("chair"))
+print(translate("rhythm"))
