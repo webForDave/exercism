23d48dbca2065d9a07e31c514035878cbbf7e38e
David Akinola <dakinola54@gmail.com>
2025-05-11 15:12:29 +0100

    starting over

--- braces.py
+++ braces.py
@@ -1,22 +1,2 @@
 def is_paired(input_string):
-    open_curly_braces = close_curly_braces = open_parenthesis = close_parenthesis = open_brackets = close_brackets = 0
-
-    for char in input_string:
-        if char == '{':
-            open_curly_braces += 1
-        elif char == '}':
-            close_curly_braces += 1
-        elif char == '(':
-            open_parenthesis += 1
-        elif char == ')':
-            close_parenthesis += 1
-        elif char == '[':
-            open_brackets += 1
-        elif char == ']':
-            close_brackets += 1
-
-    if open_curly_braces == close_curly_braces and open_parenthesis == close_parenthesis and open_brackets == close_brackets:
-        return True
-    return False
-
-print(is_paired("{hello ([agent672)}"))
+    pass
