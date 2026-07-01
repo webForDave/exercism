ca222b5a05e392ab15c79e17d8824c792e8d6995
David Akinola <dakinola54@gmail.com>
2026-07-01 22:57:08 +0100

    Updated commentary

--- colatz_conjecture/script.py
+++ colatz_conjecture/script.py
@@ -1,4 +1,19 @@
 def steps(number):
+    """Calculates the number of steps it takes to reach 1 
+    according to the rules of the Collatz Conjecture.
+
+    Parameter:
+        number (int): The number to run the puzzle on.
+
+    Returns:
+        error: If the number is less than 1.
+        int: The number of steps taken on the number.
+
+    Examples: 
+        >>> steps(12)
+        9
+    
+    """
     total_steps = 0
 
     if number < 1:
@@ -12,5 +27,3 @@ def steps(number):
             number = (number * 3) + 1
     
     return total_steps
-
-print(steps(0))
