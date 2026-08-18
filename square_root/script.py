35c22dae18e573dae3089b95f4d698496ee191e5
David Akinola <dakinola54@gmail.com>
2026-08-18 22:49:15 +0100

    Square_root

---
+++ square_root/script.py
@@ -0,0 +1,17 @@
+"""
+Calculates the square root of a given number
+"""
+
+def square_root(number):
+    """
+    Parameters:
+        number (int): Given natural number
+
+    Returns:
+        int: Calculated natural number square root
+
+    Examples:
+    >>> square_root()
+    """
+
+    return int(number ** (0.5))
